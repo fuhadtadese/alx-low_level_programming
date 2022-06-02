af9433d9475d0b37e7bb106263f97d85679aa0ff
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-02 16:32:39 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#include <unistd.h>
-
-
-
-/*
- * main - Entry point
+/**
  *
- * description:sonething
+ *  * main - Entry point
+ *
+ *   *
+ *
+ * description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
  *     *
  *
- *      * Return: Always 0 (Success)
+ ** Return: Always 1 (Success)
+ *
+ *  *
  *
  */
-
-
 
 int main(void)
 
 {
-
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 
