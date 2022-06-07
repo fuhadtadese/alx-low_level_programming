dd096863a805098d23b3ebc922aaaf1695c0aa81
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-07 03:39:08 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: to print putchar
+ *
+ * return: Always (0) Success
+ */
+int main(void)
+{
+printf("_putchar");
+
+return(0);
+}
