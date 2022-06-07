a13d0e3d41cefe0c56ffbd9675fff02c6feb3c71
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-07 06:27:18 -0700

    the second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ *print_alphabet - print alphabet in lowercase
+ *
+ * return: Always (0)
+ */
+void print_alphabet(void)
+{
+int a = 97;
+do {
+_putchar(a++);
+} while(a < 123);
+}
+
