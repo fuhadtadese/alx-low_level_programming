4ecb61da047b1a46498bba42c599439508396278
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-09 11:55:17 -0700

    push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,51 @@
 /**
  * print_last_digit - prints last digit
  *
+ *@n: arguement to function
+ *
  * return: last digit.
  */
 int print_last_digit(int n)
 {
-return (n % 10);
+if (n == 0)
+{
+_putchar (48);
+}
+else if (n == 1)
+{
+_putchar (49);
+}
+else if (n == 2)
+{
+_putchar (50);
+}
+else if (n == 3)
+{
+_putchar (51);
+}
+else if (n == 4)
+{
+_putchar (52);
+}
+else if (n == 5)
+{
+_putchar (53);
+}
+else if (n == 6)
+{
+_putchar (54);
+}
+else if (n == 7)
+{
+_putchar (55);
+}
+else if (n == 8)
+{
+_putchar (56);
+}
+else
+{
+_putchar (57);
+}
+return (n);
 }
