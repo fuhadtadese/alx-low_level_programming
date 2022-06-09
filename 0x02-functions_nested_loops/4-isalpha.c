ea43c4a6f49872a8eb6b6ab6c35f586451bcb6df
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-09 03:39:32 -0700

    push

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,19 @@
 /**
  * _isalpha - returns 1 for all alphabet
  *
- * return: 1 for alphabet 0 for others
+ *@c: argument for function
+ *
+ * Return: 1 for alphabet 0 for others
  */
 int _isalpha(int c)
 {
 if (c >= 65 && c <= 122)
 {
 return(1);
-} else{
+}
+else
+{
 return(0);
 }
-_putchar('\n');
+_putchar ('\n');
 }
