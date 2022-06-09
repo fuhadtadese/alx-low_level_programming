d4cdbf8a4f2e6f54e79230da772b921e1e81023b
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-09 15:14:49 -0700

    push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,39 +14,39 @@ if (m == 0)
 {
 _putchar (48);
 }
-else if (m == 1 ||m == -1)
+else if (m == 1 || m == -1)
 {
 _putchar (49);
 }
-else if (m == 2 ||m == -2)
+else if (m == 2 || m == -2)
 {
 _putchar (50);
 }
-else if (m == 3 ||m == -3)
+else if (m == 3 || m == -3)
 {
 _putchar (51);
 }
-else if (m == 4 ||m == -4)
+else if (m == 4 || m == -4)
 {
 _putchar (52);
 }
-else if (m == 5 ||m == -5)
+else if (m == 5 || m == -5)
 {
 _putchar (53);
 }
-else if (m == 6 ||m == -6)
+else if (m == 6 || m == -6)
 {
 _putchar (54);
 }
-else if (m == 7 ||m == -7)
+else if (m == 7 || m == -7)
 {
 _putchar (55);
 }
-else if (m == 8 ||m == -8)
+else if (m == 8 || m == -8)
 {
 _putchar (56);
 }
-else if (m == 9 ||m == -9)
+else if (m == 9 || m == -9)
 {
 _putchar (57);
 } else
