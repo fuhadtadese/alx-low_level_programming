6952f45db3d6874470be667018719ddbf55ca79d
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-14 10:47:48 -0700

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * reset_to_98 - resets value to 98
+ *
+ * @n: argument to n
+ *
+ * Return: Always 0 success.
+ */
+void reset_to_98(int *n);
+void reset_to_98(int *n)
+{
+*n = 98;
+}
