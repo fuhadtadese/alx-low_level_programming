9a8ef356e709d90e51734e850ce3234490bf1a98
fuhadtadese <fuhad.m1901765@st.futminna.edu.ng>
2022-06-15 02:22:38 -0700

    second

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/*
+ * swap_int - swaps integer
+ *
+ * @a: first argument for function
+ *
+ * @b: second argument for function
+ *
+ * Return: A swapped integers
+ */
+void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
+{
+int *a;
+a = &b;
+int *b;
+b = &a;
+}
